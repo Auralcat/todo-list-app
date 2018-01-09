17e70cd6c111d4c735631344ea9dbfccdaed8580
Auralcat <ashleyzt@hi2.in>
2018-01-09 14:19:26 -0200

    Downgrade Django version

--- todo_list_app/settings/base.py
+++ todo_list_app/settings/base.py
@@ -129,5 +129,6 @@ REST_FRAMEWORK = {
     'DEFAULT_MODEL_SERIALIZER_CLASS':
         'rest_framework.serializers.ModelSerializer',
     'DEFAULT_PERMISSION_CLASSES':
-        ('rest_framework.permissions.IsAdminUser',)
+        ('rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser'),
 }
